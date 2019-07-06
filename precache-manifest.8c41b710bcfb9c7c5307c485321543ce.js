802574560bf3aebc2f5f05758de0b8b115cad14a
wd_CN <weidong@gwu.edu>
2019-07-06 00:08:10 -0400

    Updates

---
+++ precache-manifest.8c41b710bcfb9c7c5307c485321543ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdaa21ad24e390852c6a47301bbd70c",
+    "url": "/testgitpage/index.html"
+  },
+  {
+    "revision": "0c5c2f93546025265726",
+    "url": "/testgitpage/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testgitpage/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0c5c2f93546025265726",
+    "url": "/testgitpage/static/js/main.421652ee.chunk.js"
+  },
+  {
+    "revision": "8343adff4f51140e0235",
+    "url": "/testgitpage/static/js/runtime~main.795d7f76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testgitpage/static/media/logo.5d5d9eef.svg"
+  }
+]);

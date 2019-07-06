5c366f7a8c7e39ea804534c9b2c9a61035c1691b
wd_CN <weidong@gwu.edu>
2019-07-06 12:04:13 -0400

    addf etch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+interface Todo { 
+  complete: boolean;
+  id: number;
+  userId: number;
+  title: string;
+}
 function App() {
+  const [data, setData] = useState<Todo>({ complete: true, id: 1, userId: 1, title:'empty'});
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => setData(json))
+    return () => {
+
+    };
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
+
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,11 +31,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         
+
         </a>
-       
+
       </header>
-      <h1>this is the test of my personal website</h1>
+      <h1>{data.title}</h1>
     </div>
   );
 }
